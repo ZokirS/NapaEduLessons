bbf3d10c1d11dd28390e321643f4c78431414e34
Zokir Sobirjonov <zokir5857@gmail.com>
2024-02-10 20:38:40 +0500

    uncomment above code

--- WebProgramming/Program.cs
+++ WebProgramming/Program.cs
@@ -4,36 +4,36 @@ using System.Text.Json;
 using WebProgramming;
 using WebProgramming.Models;
 
-//var uri = "https://jsonplaceholder.typicode.com";
+var uri = "https://jsonplaceholder.typicode.com";
 
-//using (var client = new HttpClient())
-//{
-//    //var message = new HttpRequestMessage(HttpMethod.Get, "https://jsonplaceholder.typicode.com/");
-//    var content = new StringContent("{\"title\":\"foo\",\"body\": \"bar\",\"userId\": 1}", Encoding.UTF8, "application/json");
-//    var result = client.PostAsync("/posts", content);
-//    Console.WriteLine(result.Result.Content.ReadAsStringAsync().Result);
-//}
+using (var client = new HttpClient())
+{
+    //var message = new HttpRequestMessage(HttpMethod.Get, "https://jsonplaceholder.typicode.com/");
+    var content = new StringContent("{\"title\":\"foo\",\"body\": \"bar\",\"userId\": 1}", Encoding.UTF8, "application/json");
+    var result = client.PostAsync("/posts", content);
+    Console.WriteLine(result.Result.Content.ReadAsStringAsync().Result);
+}
 
-//using (var client = new HttpClient())
-//{
-//    client.BaseAddress = new Uri("https://jsonplaceholder.typicode.com/");
-//    var getResult = client.GetAsync("posts");
-//    Console.WriteLine(getResult.Result.Content.ReadAsStringAsync().Result);
-//}
+using (var client = new HttpClient())
+{
+    client.BaseAddress = new Uri("https://jsonplaceholder.typicode.com/");
+    var getResult = client.GetAsync("posts");
+    Console.WriteLine(getResult.Result.Content.ReadAsStringAsync().Result);
+}
 
-//using (var client = new HttpClient())
-//{
-//    var model = new ModelClass
-//    {
-//        Title = "foo",
-//        Body = "bar",
-//        UserId = 1
-//    };
-//    var json = JsonSerializer.Serialize(model);
-//    var content = new StringContent(json, Encoding.UTF8, "application/json");
-//    var res = client.PostAsync($"{uri}/posts", content).Result;
-//    Console.WriteLine(res.Content.ReadAsStringAsync().Result);
-//}
+using (var client = new HttpClient())
+{
+    var model = new ModelClass
+    {
+        Title = "foo",
+        Body = "bar",
+        UserId = 1
+    };
+    var json = JsonSerializer.Serialize(model);
+    var content = new StringContent(json, Encoding.UTF8, "application/json");
+    var res = client.PostAsync($"{uri}/posts", content).Result;
+    Console.WriteLine(res.Content.ReadAsStringAsync().Result);
+}
 var policy = new SnakeCaseNamingPolicy();
 using (var client = new HttpClient())
 {
